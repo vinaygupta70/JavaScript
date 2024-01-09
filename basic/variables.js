d996e0b75e9d2c6a336b976eccaf792207282b4f
vinaygupta70 <90139703+vinaygupta70@users.noreply.github.com>
2024-01-09 18:19:28 +0000

    config done

---
+++ basic/variables.js
@@ -0,0 +1,12 @@
+const accountId="1234"
+let accountName="Vinay"
+var accountcity="Noida"
+
+//const can't be changed once its declared..
+
+accountName="Vinay Gupta"
+accountcity="Gr. Noida"
+
+// console.log(accountName);
+
+console.table([accountId, accountName, accountcity]);
