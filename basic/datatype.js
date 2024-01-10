322fcbc5dff9199e0e680d56fcf4e413cd48416c
vinaygupta70 <90139703+vinaygupta70@users.noreply.github.com>
2024-01-10 17:57:11 +0000

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,9 @@
+let name="Vinay"          //string
+let age= 23               //number
+let isloggedIn= true       // boolean
+let hobby;                 //undefined
+
+let favSport= null;       //null , that is standalone value
+
+console.log(typeof ("vinay"));
+
