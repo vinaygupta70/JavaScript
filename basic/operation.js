53a895035a605430ccc320a883f25d61a17412b0
vinaygupta70 <90139703+vinaygupta70@users.noreply.github.com>
2024-03-20 11:23:29 +0000

    operation

---
+++ basic/operation.js
@@ -0,0 +1,14 @@
+let value=33
+let negValue=-value
+//console.log(negValue);
+
+let str1="vinay"
+let str2=" Gupta"
+let str3 = str1 + str2
+//console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log("1" + "2" + 3);
+console.log(1 + 2 + "3");
